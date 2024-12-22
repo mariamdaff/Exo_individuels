f448ea2c24c5d035f89547840851428d7b779b1d
Mariam <daffmariam8@gmail.com>
2024-12-23 00:57:53 +0100

    ajout de quelques commentaires

--- bonjour_JavaScript/script.js
+++ bonjour_JavaScript/script.js
@@ -1,37 +1,34 @@
-// Etape 1, 2 et 3
+// Exo "bonjour javaScript"
 let message = "Bonjour !";         // declaration d'une variable 
-let hour = new Date().getHours();
- const h1 =document.querySelector('h1')
- let firstName; //= prompt('Entrez votre prénom :');
+let hour = new Date().getHours(); // recupere l'heure actuelle
+ const h1 =document.querySelector('h1')  // recupere le titre h1
+ let firstName; 
 
- function demanderUtilisateur() {
-    let firstName;
-    do {
-        firstName = prompt('Entrez votre prénom :').trim();
-    } while (!firstName || !isNaN(firstName)); // Vérifie si l'entrée est vide ou un nombre
-    return firstName;
+ function demanderUtilisateur() {         //declaration d'une fonction qui demande à l'utilisateur
+    do {                                  // do veut dire fait
+        firstName = prompt('Entrez votre prénom :').trim(); // demande à l'utilisateur de rentrer un prenom
+    } while (!firstName || !isNaN(firstName)); // tant que  l'entrée est vide ou est un nombre (invalide)
+    return firstName;  // sinon retoune le prenom ( valide)
 }
 
-
-
-function sayHello(firstName, hour){
-    if(hour >= 18){
-        message = `Bonsoir ${firstName} !`; //réassignation de la variable message
+function sayHello(firstName, hour){  // fonction qui dit bonjour 
+    if(hour >= 18){                  // si l'heure est superieure ou egale à 18
+        message = `Bonsoir ${firstName} !`; // dit bonsoir firsname
         h1.innerText = message;
         console.log(message);
         return message;
-    }else{
-        message = `Bonjour ${firstName} !`;
+    }else{                                    // sinnon 
+        message = `Bonjour ${firstName} !`;   // dit bonjour
          console.log(message)
-        h1.innerText = message;
+        h1.innerText = message; // injecte dynamiquement du texte dans h1
     }
 }
-firstName = demanderUtilisateur();
-sayHello(firstName, hour);
+firstName = demanderUtilisateur();  //reassignation de la variable firsname
+sayHello(firstName, hour);         // appelle de la fonction sayHello
+// mettre un peu de syle sur ma page
 h1.style.textAlign = 'center';
 h1.style.fontSize = '3em';
 h1.style.animation = 'fadeIn 5s ease-in-out';
-
 document.querySelector('body').style.background = 'linear-gradient(135deg,rgb(151, 220, 250),rgb(15, 155, 202),rgb(7, 91, 128))';
 
 
